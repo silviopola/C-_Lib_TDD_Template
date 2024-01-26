306743b624fd18d177830952aaabcae92cb2a8dc
Silvio Pola <silvio.pola@gmail.com>
2024-01-26 17:13:21 +0100

    First Commit

---
+++ ClassLibrary.Test/Class1Fixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary.Test
+{
+    public class Class1Fixture
+    {
+        private readonly Class1 _class1;
+
+        public Class1Fixture()
+        {
+            _class1 = new Class1();
+        }
+
+        [Test]
+        public void Method1_returns_result()
+        {
+            var result = _class1.Method1(true);
+            
+            Assert.That(result, Is.EqualTo(true));
+        }
+    }
+}
---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public bool Method1(bool result)
+        {
+            return result;
+        }
+    }
+}
